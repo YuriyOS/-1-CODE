dc8308c85c59a06c9d307c3432254313360857b2
yuriyos <yuriy.osenin@gmail.com>
2020-05-15 22:43:09 +0300

    part 5

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,5 @@
-const nextIcon = '<img class="right-arrow" src="/images/slider-icons/right-arrow.png" alt="right">';
-const prevIcon = '<img  class="left-arrow" src="/images/slider-icons/left-arrow.png" alt="left" >';
-
-document.addEventListener("DOMContentLoaded", function() {
-	document.querySelector('.trust-us__slider').owlCarousel({
-
-    items:3,
-
-    loop:true,
-    
-	})
+$(function(){
+    $('.header__menu-btn').on('click', function(){
+        $('.navigation').slideToggle();
+    });
 });
-
-
-
-
