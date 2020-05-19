823bed3da031320842cbbadaadf58057bc45db25
yuriyos <yuriy.osenin@gmail.com>
2020-05-19 04:12:53 +0300

    part 5

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,43 @@
+//= ../../node_modules/owl.carousel2/dist/owl.carousel.js
+
 $(function(){
     $('.header__menu-btn').on('click', function(){
         $('.navigation').slideToggle();
     });
+
+    $('.owl-carousel').owlCarousel({
+
+        center: true,
+        loop: true,
+
+        responsive:{
+            0:{                
+                items:3
+            },
+            768:{
+                items:4
+            }
+            
+        }
+
+    });
+
+    $('.reviews__slider').owlCarousel({
+
+        center: true,
+        // loop: true,
+
+        responsive:{
+            0:{                
+                items:2
+            },
+            768:{
+                items:4
+            }
+            
+        }
+
+    });
+
+
 });
