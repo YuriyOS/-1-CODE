5ba9de7754e2a08aa687b5a43ed29d0f2ed4b25d
yuriyos <yuriy.osenin@gmail.com>
2020-05-31 22:58:47 +0300

    Modal Completed

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -7346,7 +7346,12 @@ $(function () {
 
   function createModal() {
     var sTitle = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Заповніть форму';
-    $('body').prepend('' + '<div class="modal">\n' + '    <div class="modal__content">\n' + '        <div class="btn-close"></div>\n' + '        <h2 class="heading-style-1">' + sTitle + '</h2>\n' + '        <form class="form modal__form">        \n' + '            <!-- Hidden Required Fields -->\n' + '            <input type="hidden" name="project_name" value="Site Name">\n' + '            <input type="hidden" name="admin_email" value="admin@mail.com">\n' + '            <input type="hidden" name="form_subject" value="Form Subject">\n' + '            <!-- END Hidden Required Fields -->\n' + '            <input class="input modal__input main-text" type="text" name="Name" placeholder="Ваше ім\'я..." required><br>\n' + '            <input class="input modal__input main-text" type="text" name="E-mail" placeholder="Ваше E-mail..." required><br>\n' + '            <input class="input modal__input main-text" type="text" name="Phone" placeholder="Ваш телефон..."><br>\n' + '            <button class="form__btn style-btn reset-btn">ЗАМОВИТИ</button>\n' + '        </form>\n' + '        <p class="main-text main-text_white">Ми НЕ передаємо Ваші дані 3-м особам.</p>\n' + '    </div>\n' + '</div>');
+    $('body').prepend('' + '<div class="modal">\n' + '    <div class="modal__dialog">\n' + '        <div class="btn-close"></div>\n' + '        <h2 class="heading-style-1">' + sTitle + '</h2>\n' + '        <form class="form modal__form">        \n' + // '            <!-- Hidden Required Fields -->\n' +
+    // '            <input type="hidden" name="project_name" value="Site Name">\n' +
+    // '            <input type="hidden" name="admin_email" value="admin@mail.com">\n' +
+    // '            <input type="hidden" name="form_subject" value="Form Subject">\n' +
+    // '            <!-- END Hidden Required Fields -->\n' +
+    '            <input class="input modal__input main-text" type="text" name="Name" placeholder="Ваше ім\'я..." required><br>\n' + '            <input class="input modal__input main-text" type="text" name="E-mail" placeholder="Ваше E-mail..." required><br>\n' + '            <input class="input modal__input main-text" type="text" name="Phone" placeholder="Ваш телефон..."><br>\n' + '            <button class="form__btn style-btn reset-btn">ЗАМОВИТИ</button>\n' + '        </form>\n' + '        <p class="main-text main-text_white">Ми НЕ передаємо Ваші дані 3-м особам.</p>\n' + '    </div>\n' + '</div>');
   }
 
   $('.btn-form').on('click', function (e) {
@@ -7354,11 +7359,13 @@ $(function () {
     $('.modal').fadeIn();
     $('body').addClass('body_fixed');
     $('body').prepend('<div class="overlay"></div>');
-    $('.overlay').on('click', function () {
-      $('.modal').fadeOut();
-      $('.overlay').remove();
-      $('.modal').remove();
-      $('body').removeClass('body_fixed');
+    $('.modal').on('click', function (e) {
+      if (!$(e.target).closest('.modal__dialog').length && !$(e.target).is('.modal__dialog')) {
+        $('.modal').fadeOut();
+        $('.overlay').remove();
+        $('.modal').remove();
+        $('body').removeClass('body_fixed');
+      }
     });
     $('.btn-close').on('click', function () {
       $('.modal').fadeOut();
